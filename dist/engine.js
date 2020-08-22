e9220e0d6e167caec51cc243c1dffc80da5e702d
alanlh <alan.leyi.hu@gmail.com>
2020-08-22 10:17:02 -0500

    Virtual dom optimization

--- dist/engine.js
+++ dist/engine.js
@@ -2058,7 +2058,7 @@ var AsciiEngine = (function () {
         this.elements.push([]);
         this.activeRowLength.push(0);
         for (let x = 0; x < width; x ++) {
-          this.elements[y].push(document.createElement("span"));
+          this.elements[y].push(new DOMCellWrapper());
         }
       }
     }
@@ -2081,11 +2081,11 @@ var AsciiEngine = (function () {
     setRowLength(row, length) {
       if (length < this.activeRowLength[row]) {
         for (let x = this.activeRowLength[row] - 1; x >= length; x --) {
-          this.rows[row].removeChild(this.elements[row][x]);
+          this.rows[row].removeChild(this.elements[row][x].getDomElement());
         }
       } else if (length > this.activeRowLength[row]) {
         for (let x = this.activeRowLength[row]; x < length; x ++) {
-          this.rows[row].appendChild(this.elements[row][x]);
+          this.rows[row].appendChild(this.elements[row][x].getDomElement());
         }
       }
       this.activeRowLength[row] = length;
@@ -2096,7 +2096,7 @@ var AsciiEngine = (function () {
         let x = 0;
         let cellsUsed = 0;
         while (x < this.width) {
-          let domElement = this.elements[y][cellsUsed];
+          let domElementWrapper = this.elements[y][cellsUsed];
           
           let segmentLength = drawBuffer.getSegmentLengthAt(x, y);
           let segmentData = drawBuffer.getSegmentAt(x, y);
@@ -2112,21 +2112,71 @@ var AsciiEngine = (function () {
               segmentLength
             );
           }
-          domElement.textContent = text;
+          domElementWrapper.setText(text);
           
           let style = segmentData.styles;
           for (let styleName in style) {
-            domElement.style[styleName] = style[styleName] || "";
+            domElementWrapper.setStyle(styleName, style[styleName]);
           }
-          
-          domElement.dataset.asciiGlId = segmentData.frontId;
+
+          domElementWrapper.setId(segmentData.frontId);
 
           cellsUsed ++;
           x += segmentLength;
+          domElementWrapper.applyChanges();
         }
         
         this.setRowLength(y, cellsUsed);
       }
+    }
+  }
+
+  class DOMCellWrapper {
+    constructor() {
+      this._domElement = document.createElement("span");
+      this._currText = "";
+      this._currStyles = {};
+
+      this._nextText = "";
+      this._nextStyles = {};
+
+      this._currId = "";
+      this._nextId = "";
+    }
+    
+    getDomElement() {
+      return this._domElement;
+    }
+
+    setText(value) {
+      this._nextText = value;
+    }
+
+    setStyle(styleName, value) {
+      this._nextStyles[styleName] = value || "";
+    }
+
+    setId(value) {
+      this._nextId = value;
+    }
+
+    applyChanges() {
+      let domElement = this._domElement;
+      if (this._nextText !== this._currText) {
+        domElement.textContent = this._nextText;
+      }
+      this._currText = this._nextText;
+      for (let styleName in this._nextStyles) {
+        let styleValue = this._nextStyles[styleName];
+        if (styleValue !== this._currStyles[styleName]) {
+          domElement.style[styleName] = styleValue;
+        }
+        this._currStyles[styleName] = styleValue;
+      }
+      if (this._nextId !== this._currId) {
+        domElement.dataset.asciiGlId = this._nextId;
+      }
+      this._currId = this._nextId;
     }
   }
 
--- src/graphics/DomBuffer.js
+++ src/graphics/DomBuffer.js
@@ -25,7 +25,7 @@ export default class DOMBuffer {
       this.elements.push([]);
       this.activeRowLength.push(0);
       for (let x = 0; x < width; x ++) {
-        this.elements[y].push(document.createElement("span"));
+        this.elements[y].push(new DOMCellWrapper());
       }
     }
   }
@@ -48,11 +48,11 @@ export default class DOMBuffer {
   setRowLength(row, length) {
     if (length < this.activeRowLength[row]) {
       for (let x = this.activeRowLength[row] - 1; x >= length; x --) {
-        this.rows[row].removeChild(this.elements[row][x]);
+        this.rows[row].removeChild(this.elements[row][x].getDomElement());
       }
     } else if (length > this.activeRowLength[row]) {
       for (let x = this.activeRowLength[row]; x < length; x ++) {
-        this.rows[row].appendChild(this.elements[row][x]);
+        this.rows[row].appendChild(this.elements[row][x].getDomElement());
       }
     } else {
       // If equal, do nothing.
@@ -65,7 +65,7 @@ export default class DOMBuffer {
       let x = 0;
       let cellsUsed = 0;
       while (x < this.width) {
-        let domElement = this.elements[y][cellsUsed];
+        let domElementWrapper = this.elements[y][cellsUsed];
         
         let segmentLength = drawBuffer.getSegmentLengthAt(x, y);
         let segmentData = drawBuffer.getSegmentAt(x, y);
@@ -81,20 +81,70 @@ export default class DOMBuffer {
             segmentLength
           );
         }
-        domElement.textContent = text;
+        domElementWrapper.setText(text);
         
         let style = segmentData.styles;
         for (let styleName in style) {
-          domElement.style[styleName] = style[styleName] || "";
+          domElementWrapper.setStyle(styleName, style[styleName]);
         }
-        
-        domElement.dataset.asciiGlId = segmentData.frontId;
+
+        domElementWrapper.setId(segmentData.frontId);
 
         cellsUsed ++;
         x += segmentLength;
+        domElementWrapper.applyChanges();
       }
       
       this.setRowLength(y, cellsUsed);
     }
+  }
+}
+
+class DOMCellWrapper {
+  constructor() {
+    this._domElement = document.createElement("span");
+    this._currText = "";
+    this._currStyles = {};
+
+    this._nextText = "";
+    this._nextStyles = {};
+
+    this._currId = "";
+    this._nextId = "";
+  }
+  
+  getDomElement() {
+    return this._domElement;
+  }
+
+  setText(value) {
+    this._nextText = value;
+  }
+
+  setStyle(styleName, value) {
+    this._nextStyles[styleName] = value || "";
+  }
+
+  setId(value) {
+    this._nextId = value;
+  }
+
+  applyChanges() {
+    let domElement = this._domElement;
+    if (this._nextText !== this._currText) {
+      domElement.textContent = this._nextText;
+    }
+    this._currText = this._nextText;
+    for (let styleName in this._nextStyles) {
+      let styleValue = this._nextStyles[styleName];
+      if (styleValue !== this._currStyles[styleName]) {
+        domElement.style[styleName] = styleValue;
+      }
+      this._currStyles[styleName] = styleValue;
+    }
+    if (this._nextId !== this._currId) {
+      domElement.dataset.asciiGlId = this._nextId;
+    }
+    this._currId = this._nextId;
   }
 }
